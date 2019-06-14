a63c96b31a2566b2c4cac69852782e557fe76826
Izaflps <izaflps@gmail.com>
2019-06-14 20:05:59 -0300

    primeiro commit

---
+++ DozePontoDois/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DozePontoDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qntalunos;
+            float media, maior=0, menor=10, soma=0;
+            
+            Console.WriteLine("Quantos alunos tem na turma?");
+            qntalunos = int.Parse(Console.ReadLine());
+           
+            float[] nota = new float[qntalunos];
+            for (int i = 0; i < qntalunos; i++)
+            {
+                Console.WriteLine("Informe a nota do aluno " + i + ": ");
+                nota[i] = float.Parse(Console.ReadLine());
+                soma = soma + nota[i];
+                if (nota[i] > maior)
+                {
+                    maior = nota[i];
+                }
+                if (nota[i] < menor)
+                {
+                    menor = nota[i];
+                }
+            }
+            media = soma / qntalunos;
+            Console.WriteLine("A media é: " + media + ".");
+            Console.WriteLine("A maior nota é: " + maior + ".");
+            Console.WriteLine("A menor nota é: " + menor + ".");
+
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
